6714d5f253a5d0f907f9c240bdcccae33a921d60
Zuriel V. <zuriivalentin@gmail.com>
2025-08-23 22:37:03 -0600

    Update ReportCommand.php

--- src/madversal/simplereport/commands/ReportCommand.php
+++ src/madversal/simplereport/commands/ReportCommand.php
@@ -16,7 +16,7 @@ final class ReportCommand extends Command implements PluginOwned {
 
     public function __construct(Main $plugin) {
         parent::__construct("report", "Report a player", "/report <player> <reason>");
-        $this->owningPlugin = $plugin;
+        $this->owningPlugin(); = $plugin;
         $this->setPermission("simplereport.use");
     }
 
